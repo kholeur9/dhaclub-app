2f1ab00ea7a954ec94a250fbf0268327e57bc04b
kholeur9 <65097234+kholeur9@users.noreply.github.com>
2026-05-25 19:57:35 +0100

    Update codes.go

--- internal/apperrors/codes.go
+++ internal/apperrors/codes.go
@@ -2,8 +2,17 @@ package apperrors
 
 import "errors"
 
+// USER
 var (
-	ErrUserNotFound       = errors.New("user not found")
-	ErrManyFieldsToUpdate = errors.New("many fields to update")
-	ErrNoFieldsToUpdate   = errors.New("no fields to update")
+	ErrUserNotFound = errors.New("user not found")
+	ErrManyFieldsToUpdate = errors.New("many fields found")
+	ErrNoFieldsToUpdate = errors.New("no data found")
+)
+
+// TODO
+var (
+	ErrTodoExists           = errors.New("todo already exists")
+	ErrTodoNotFound         = errors.New("todo not found")
+	ErrTodoTooShort         = errors.New("description too short")
+	ErrDescriptionTodoEmpty = errors.New("description is empty")
 )
