4771c96e996f627302ed979814feb6b21ecdcc2b
kholeur9 <65097234+kholeur9@users.noreply.github.com>
2026-05-25 20:24:48 +0100

    Update messages.go

--- internal/apperrors/messages.go
+++ internal/apperrors/messages.go
@@ -12,9 +12,6 @@ const (
 	PasswordShort        = "Password must be at least 8 characters"
 	EmailAlreadyExists   = "Email already exists"
 	UsernameAlreadyExists = "Username already exists"
- ErrUserNotFoundMessage = "User not found"
-	EmailNotChanged    = "Email has not changed"
-	UsernameNotChanged = "Username has not changed"
  OneFieldToUpdate = "You can only update one field at a time"
  NoDataToUpdate   = "No data to update"
 )
