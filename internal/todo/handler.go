33b49593124058f29737773700bb2c48a7db4ae9
kholeur9 <yannewilfriedm@gmail.com>
2026-06-24 01:13:08 +0100

    Refactor TodosListHandler to extract pagination parameters from the request URL; enhance logging for page and limit values.

--- internal/todo/handler.go
+++ internal/todo/handler.go
@@ -1,7 +1,10 @@
 package todo
 
 import (
-	//"fmt"
+	"fmt"
+	//"strconv"
+	//"strings"
+
 	//"context"
 	"encoding/json"
 
@@ -84,6 +87,11 @@ func (s *HandlerTodo) GetTodoByIDHandler(w http.ResponseWriter, r *http.Request)
 }
 
 func (s *HandlerTodo) TodosListHandler(w http.ResponseWriter, r *http.Request) {
+	getURL := r.URL.Query()
+	//pageNumber := strings.Split(strings.Split(strings.Split(getURLpage, "/todos?")[1], "&")[0], "=")[1]
+	page := getURL["page"][0]
+	limit := getURL["limit"][1]
+	fmt.Println(page, limit)
 	userIDContext := r.Context().Value(shared.UserIDKey)
 	if userIDContext == nil {
 		response.HandleServiceError(w, &apperrors.ServiceError{
