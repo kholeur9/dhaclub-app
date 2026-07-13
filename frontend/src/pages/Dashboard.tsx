c61e5430d8baffc08a4d8f34239410017601d4e6
kholeur9 <yannewilfriedm@gmail.com>
2026-07-13 06:58:04 +0100

    refactor: restructure Dashboard component to manage todos state and simplify todo addition

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,24 +1,32 @@
-//import { useState } from "react";
+import { useState } from "react";
 import {TodosList} from "../components/TodosList";
 import type { Todo } from "../types/todo";
 
 function Dashboard() {
-  const todo1: Todo = {
-    id: "1",
-    description: "MY FIRST TODO",
-    completed: false,
-    createdAt: new Date(),
-  }
+  const [todos, setTodos] = useState<Todo[]>([
+    {
+      id: "1",
+      description: "MY FIRST TODO",
+      completed: false,
+      createdAt: new Date(),
+    }
+  ])
+
   const todo2: Todo = {
     id: "2",
     description: "LEARN REACT",
     completed: true,
     createdAt: new Date(),
   }
-  const todos: Todo[] = [todo1, todo2];
+
+  const addTodo = () => {
+    setTodos((prev) => [...prev, todo2])
+  }
+  
   return (
     <>
       <h1>Dashboard MIRANDE</h1>
+      <button onClick={addTodo}>+ add a todo</button>
       <TodosList list={todos} />
     </>
   );
